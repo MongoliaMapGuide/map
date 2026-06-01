8a3bda6cff1f4d0a540dc913e5619080e1056a5e
BayarChoijil <135679844+BayarChoijil@users.noreply.github.com>
2026-06-02 02:58:32 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'travelmap-v71';
+const CACHE_NAME = 'travelmap-v72';
 
 const urlsToCache = [
   '/',
@@ -6,6 +6,7 @@ const urlsToCache = [
   '/manifest.json',
   '/logo192.png',
   '/logo512.png',
+  '/logo180.png',
   '/logo1200x630.png',
   '/favicon.ico'
 ];
